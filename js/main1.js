5122129b89cf5ca32da0d28a22d656bde0da73a8
Atiye <aty.sokhansanj@gmail.com>
2020-04-29 02:21:14 +0430

    add .

--- js/main1.js
+++ js/main1.js
@@ -1,35 +1,27 @@
 
-(function ($) {
+$(document).ready(function () {
 	$('.dropdown-menu li.dropdown').on('click', function (e) {
 		$(this).toggleClass("show");
 		var $subMenu = $(this).find('ul:nth-child(1)').addClass('show');
 		$subMenu.toggleClass('show');
 
-		// $(this).parents('.dropdown-menu.show').on('hidden.bs.dropdown', function (e) {
-		// 	// $('.show').removeClass("show");
-		// });
 		return false;
 	});
 
-	 var $hamburgerMenu=$('div.hamburgerMenu');
+	var $hamburgerMenu = $('div.hamburgerMenu');
 	console.log($hamburgerMenu);
 
-	$('li.dropdown-item').on('click',function(e){
+	$('li.dropdown-item').on('click', function (e) {
 		$(this).parents('ul.dropdown-menu.show').removeClass('show');
 		$(this).parents('li.nav-item.dropdown.show').removeClass('show');
 		$(this).parents('div.collapse').prev('div.hamburger-menu.navbar-toggler').addClass('collapsed');
 
-		// $(this).parents('div.collapse').prev('div.hamburger-menu.navbar-toggler').children('.navbar-toggler-icon').removeClass('change');
-		// return false;
-		// $(this).parents('div.navbar-right').children('div.hamburger-menu').addClass('collapsed')
-
-		// console.log($(this).parents('div.collapse').prev('div.hamburger-menu').toggleClass('collapsed'))
 	})
-	$('li.list-item').on('click',function(){
+	$('li.list-item').on('click', function () {
 		$(this).parents('div.navbar-right').children('div.hamburger-menu.navbar-toggler').addClass('collapsed');
 	})
 
-	$('li.list-item').on('mouseenter',function(){
+	$('li.list-item').on('mouseenter', function () {
 		$(this).parents('div.navbar-right').children('div.hamburger-menu.navbar-toggler').addClass('collapsed');
 	})
 
@@ -45,32 +37,47 @@
 		// $(this).parents('list-item.dropdown.show').removeClass('show');
 	})
 
-	// $('div.hamburger-menu').on('click',function(){
-
-	// 	$('li.dropdown-item').on('click',function(e){
-	// 		$(this).parents('ul.dropdown-menu.show').removeClass('show');
-	// 		$(this).parents('li.nav-item.dropdown.show').removeClass('show');
-	// 		$(this).find('div.hamburger-menu').addClass('collapsed');
-	// 	})
-	// })
-
-	
-})(jQuery)
-
-
-// let navItem = document.querySelector('.nav-item');
-// let dropdownMenuShow = document.querySelector('.show')
-
-let nanBarTogglerIcon = document.querySelector('.navbar-toggler-icon');
-
-nanBarTogglerIcon.addEventListener('click', function (e) {
-	nanBarTogglerIcon.classList.toggle("change");
+	//29.4.2020
 
 })
 
-// navItem.addEventListener('click', function (e) {
-    // navItem.classList.add('active');
-    // navItem.classList.remove('active')
-// })
 
-// navItem.classList.remove('active');
+let nanBarTogglerIcon = document.querySelector('.navbar-toggler-icon');
+let listItemShow = document.querySelector('li.list-item.dropdown')
+let sub22 = document.querySelector('li#sub2-2');
+let sub21 = document.querySelector('li#sub2-1');
+let navItemDropdown = document.querySelector('li#nav-item')
+
+nanBarTogglerIcon.addEventListener('click', function (e) {
+	nanBarTogglerIcon.classList.toggle("change");
+})
+
+sub22.addEventListener('click', function () {
+	sub21.classList.remove('show');
+})
+
+sub21.addEventListener('click', function () {
+	sub22.classList.remove('show');
+})
+
+navItemDropdown.addEventListener('click', function () {
+	sub21.classList.remove('show');
+	sub22.classList.remove('show');
+})
+
+sub22.addEventListener('mouseover', function () {
+	this.classList.add('show');
+})
+
+sub22.addEventListener('mouseleave', function () {
+	this.classList.remove('show');
+})
+
+
+sub21.addEventListener('mouseover', function () {
+	this.classList.add('show');
+})
+
+sub21.addEventListener('mouseleave', function () {
+	this.classList.remove('show');
+})
