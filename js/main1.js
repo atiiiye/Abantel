b132cb8d58ac412c2b04fbce5516fea9a63d1570
Atiye <aty.sokhansanj@gmail.com>
2020-04-28 21:16:01 +0430

    add changes

--- js/main1.js
+++ js/main1.js
@@ -33,6 +33,18 @@
 		$(this).parents('div.navbar-right').children('div.hamburger-menu.navbar-toggler').addClass('collapsed');
 	})
 
+
+	$('li#list-item').on('click', function (e) {
+		$(this).parents('div.collapse.navbar-collapse').toggleClass('show');
+		$(this).parents('div.navbar-right').children('div.navbar-toggler').children('span.navbar-toggler-icon').toggleClass('change')
+	})
+
+	$('li.dropdown-item').on('click', function () {
+		$(this).parents('div.collapse.navbar-collapse.show').removeClass('show');
+		$(this).parents('div.navbar-right').children('div.navbar-toggler').children('span.navbar-toggler-icon').toggleClass('change')
+		// $(this).parents('list-item.dropdown.show').removeClass('show');
+	})
+
 	// $('div.hamburger-menu').on('click',function(){
 
 	// 	$('li.dropdown-item').on('click',function(e){
