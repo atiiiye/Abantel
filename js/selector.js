c2d4acda24556db24e4dd63381739d308b729916
Atiye <aty.sokhansanj@gmail.com>
2020-05-22 06:52:22 +0430

    complete css branches in xl size

--- js/selector.js
+++ js/selector.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // let inputCheckboxes = document.querySelectorAll('input[type=checkbox]')
 
     $('input[type=checkbox]').on('click', function (e) {
-        $(this).children('input[type=checkbox]:cheked')
+        // $(this).children('input[type=checkbox]:cheked')
     })
 
     $('span.ti-angle-left').on('click', function (e) {
@@ -14,10 +14,18 @@ $(document).ready(function () {
     $('a.label-link').on('click', function (e) {
         $(this).parents('li.row').children('span.ti-angle-left').toggleClass('rotate');
         $(this).parents('div.form-check').next('ul#level-2').toggleClass('d-none');
+        $(this).parents('div.form-check').next('ul#level-3').toggleClass('d-none');
+        $(this).parents('div.form-check').next('ul#level-4').toggleClass('d-none');
+
     })
 
-    // let angleLefts = document.querySelectorAll('span.ti-angle-left')
-    // console.log(angleLefts);
+    let angleLefts = document.getElementsByClassName('ti-angle-left')
+
+    // angleLefts.addEventListener('click',function(e){
+    //         console.log('kkkk');
+            
+    //         e.classList.toggle('rotate');
+    //     })
     
 
     // angleLefts.forEach(angleLeft=>angleLeft.addEventListener('click',function(e){
