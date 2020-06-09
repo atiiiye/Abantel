135af068a9b145efbaf6aa5bcde4222d4857b8dc
Atiye <aty.sokhansanj@gmail.com>
2020-06-09 11:26:28 +0430

    edit select parents bug

--- js/selector.js
+++ js/selector.js
@@ -5,8 +5,6 @@ $(document).ready(function () {
     //                    Angle-left
     // ****************************************************************
 
-
-
     $('span.ti-angle-left').on('click', function (e) {
         $(this).toggleClass('rotate')
 
@@ -28,13 +26,9 @@ $(document).ready(function () {
     // ****************************************************************
     //                    Input checkbox
     // ****************************************************************
-    // let list2 = document.querySelectorAll('ul#level-2 li div.form-check input');
-    // let list3 = document.querySelectorAll('ul#level-3 li div.form-check input');
-    // let list4 = document.querySelectorAll('ul#level-4.list.l4')
 
     $('input[type=checkbox]').on('click', function (e) {
         if (this.checked) {
-
             $(this).parent('div.form-check').siblings('ul#level-2').children('li').children('div.form-check').children('input[type=checkbox]').each(function () {
                 this.checked = true;
             }).parent('div.form-check').siblings('ul#level-3').children('li').children('div.form-check').children('input[type=checkbox]').each(function () {
@@ -86,92 +80,147 @@ $(document).ready(function () {
             $(this).parent('div.form-check').siblings('ul#level-4').children('li').children('div.form-check').children('input[type=checkbox]').each(function () {
                 this.checked = false;
             })
-
         }
     })
 
 
-    $('ul#level-3').children('li').children('div.form-check').children('label.form-check-label').children('a.label-link').on('click', function (e) {
-        let par = $(this).parents('div.form-check')
-
-        let childL4 = par.siblings('ul#level-4').children('li').children('div.form-check').children('input');
-        if (!par.siblings('ul#level-4').hasClass('d-none')) {
-            // console.log(...childL4);
+    $('li.row.list-item').on('click', function (e) {
+        let par = $(this).children('div.form-check')
+     
+        if (!par.siblings('ul#level-2').hasClass('d-none')) {
+            let childL2 = par.siblings('ul#level-2').children('li').children('div.form-check').children('input');
+            let childRest = [...childL2]
+            const lengthItem = childRest.length
             let counter = 0;
-            var childSpread = [...childL4];
-            // console.log(childSpread);
+            
+            for (let i = 0; i < lengthItem; i++) {
+                console.log('yohoooo');
+                if(childRest[i].checked){
+                     counter++
+                     console.log(counter);
+                        if (counter === lengthItem) {
+                            let selected = $(this).children('div.form-check').children('input').prop('checked','true');
+                            console.log(selected); 
+                        }
+                    }
+                  
+
+                // childRest.forEach(item => item.addEventListener('change', function (e) {
+                    // childL2.each(function(){
+                        // for(let i=0;i< lengthItem;i++){
+                         
+                            // if (this.checked) {
+                            //     ++counter;
+                            //     console.log(counter);
+                            //     return counter   
+                            // }
+                            
+                            // if (counter === lengthItem) {
+                            //     console.log('Hello');        
+                            //     let selected = $(this).parent('div.form-check').parent('li').parent('ul#level-2').parent('li.row.list-item').children('div.form-check').children('input').prop('checked','true')
+                            //     // let selectRest=[...selected]
+                            //     // console.log(selected); 
+                            //     // selectRest.checked=true
+                            //     // console.log(selectRest);
+                            // }
+                        // }
+                    // })  
+                // }))
+            }
+        }
+    })
+
+
+    $('ul#level-2').children('li').on('click', function (e) {
+        let par2 = $(this).children('div.form-check')
+        let childL3 = par2.siblings('ul#level-3').children('li').children('div.form-check').children('input');
+        if (!par2.siblings('ul#level-3').hasClass('d-none')) {
+            let counter = 0;
+            var childRest = [...childL3];
+            const lengthItem = childRest.length
+
+            for (let i = 0; i < lengthItem; i++) {
+                console.log('yohoooo');
+                if(childRest[i].checked){
+                     counter++
+                     console.log(counter);
+                        if (counter === lengthItem) {
+                            let selected = $(this).children('div.form-check').children('input').prop('checked','true');
+                            console.log(selected); 
+                        }
+                    }
+                }
+
+
+            // for (i = 0; i < lengthItem; i++) {
+                // childRest.forEach(item => item.addEventListener('change', function (e) {
+
+                //     if (this.checked) {
+                //         counter += 1;
+                //         console.log(counter);
+                //         if (counter == lengthItem) {
+                //             let select = $(this).parent('div.form-check').parent('li').parent('ul#level-3').parent('li.sub-item').children('div.form-check').children('input').each(function () {
+                //                 this.checked = true
+                //             });
+                //             return counter
+                //         }
+                //     } else if (!this.checked) {
+                //         counter -= 1
+                //     }
+                // }))
+            // }
+
+        }
+
+    })
+
+
+    $('ul#level-3').children('li').on('click', function (e) {
+        let par3 = $(this).children('div.form-check')
+
+        let childL4 = par3.siblings('ul#level-4').children('li').children('div.form-check').children('input');
+        if (!par3.siblings('ul#level-4').hasClass('d-none')) {
+            let counter = 0;
+            var childRest = [...childL4];
+            const lengthItem = childRest.length
+
+            for (let i = 0; i < lengthItem; i++) {
+                console.log('yohoooo');
+                if(childRest[i].checked){
+                     counter++
+                     console.log(counter);
+                        if (counter === lengthItem) {
+                            let selected = $(this).children('div.form-check').children('input').prop('checked','true');
+                            console.log(selected); 
+                        }
+                    }
+                }
+            
 
             // for (i = 0; i < childSpread.length; i++) {
-            // console.log(childSpread[i]);
-            // console.log(childSpread[i].checked);
-            childSpread.forEach(item => item.addEventListener('click', function (e) {
-                // console.log(this);
+                // childSpread.forEach(item => item.addEventListener('change', function (e) {
+                //     if (this.checked) {
+                //         counter += 1;
+                //         console.log(counter);
 
-                if (this.checked) {
-                    counter += 1;
-                    console.log(counter);
-                    // console.log(childSpread.length);
-
-                    if (counter == childSpread.length) {
-                        let select = $(this).parent('div.form-check').parent('li').parent('ul#level-4').parent('li.sub-item').children('div.form-check').children('input').each(function () {
-                            this.checked = true
-                        });
-                        return counter
-                        // console.log(select);
-                    }
-                } else if (!this.checked) {
-                    counter -= 1
-                }
-            }))
+                //         if (counter == childSpread.length) {
+                //             let select = $(this).parent('div.form-check').parent('li').parent('ul#level-4').parent('li.sub-item').children('div.form-check').children('input').each(function () {
+                //                 this.checked = true
+                //             });
+                //         }
+                //     } else if (!this.checked) {
+                //         counter -= 1
+                //     }
+                // }))
             // }
         }
     })
 
 
-    $('ul#level-2').children('li').children('div.form-check').children('label.form-check-label').children('a.label-link').on('click', function (e) {
-        let par = $(this).parents('div.form-check')
-        let childL3 = par.siblings('ul#level-4').children('li').children('div.form-check').children('input');
-        console.log(childL3);
-        if (!par.siblings('ul#level-3').hasClass('d-none')) {
-            let counter = 0;
-            var childsSpread = [...childL3];
-            console.log(childsSpread);
-
-            childsSpread.forEach(item => item.addEventListener('click', function (e) {
-                // console.log(this);
-
-                if (this.checked) {
-                    counter += 1;
-                    console.log(counter);
-                    if (counter == childsSpread.length) {
-                        let select = $(this).parent('div.form-check').parent('li').parent('ul#level-3').parent('li.sub-item').children('div.form-check').children('input').each(function () {
-                            this.checked = true
-                        });
-                        return counter
-                    }
-                } else if (!this.checked) {
-                    counter -= 1
-                }
-            }))
-
-        }
-
-    })
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    
 })
